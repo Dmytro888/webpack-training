081159b66789c7e842db481263dfc80b2c9ca46b
Dmytro Orgish <isnl.dd@gmail.com>
2021-01-27 17:53:41 +0200

    start

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import style from './css/style.css';
+import countries from './data-base/coutries.json';
+import countriesTemplates from './templates/countries.hbs';
+
+const markup = countriesTemplates(countries);
+document.querySelector('ul').insertAdjacentHTML('beforeend', markup);
